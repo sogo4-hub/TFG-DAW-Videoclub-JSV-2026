186b7ac4ae24f46f29901390809f61cb2cf5ab42
sogo4 <sogo4@hotmail.com>
2026-03-05 18:32:02 +0100

    Actualizacion de la configuracion de CORS en el backend

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,11 +6,3 @@ VALUES ('nuevo_admin@streamflix.es', '$2a$10$tTPdzbYnEcqcwjLsoXqYU.ygPwl4AmqB91.
 INSERT INTO usuarios (email, password, rol)
 VALUES ('estudiante_tfg@streamflix.es', '$2a$10$SYsrYhjSfgC5FeucZO4vM.OobcBeuimSVaK8yAwBs3nLF4H82xSHm', 'USER');
 
--- Película de prueba inicial
-INSERT INTO peliculas (titulo, sinopsis, director, anio, genero, url_imagen)
-VALUES ('Interstellar',
-        'Un equipo de exploradores viaja a través de un agujero de gusano.',
-        'Christopher Nolan',
-        2014,
-        'Ciencia Ficción',
-        'https://m.media-amazon.com/images/I/A1JVqNMI7UL._AC_SY679_.jpg');
