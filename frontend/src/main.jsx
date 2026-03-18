66dee6087e4778a2837bae917316efb9eaaffc16
sara <sararuizn9@gmail.com>
2026-03-18 14:28:13 +0100

    frontend: arreglo navbar, app.jsx y main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,32 +1,17 @@
 //punto de entrasa de la app.
 //carga el html y decide q componente mostrar primero en la pantalla
 
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-//import { BrowserRouter } from 'react-router-dom' //--agregar rutas para enlaces
-import './index.css';
-import App from './App.jsx';
-import Dashboard from "./pages/dashboard/dashboard.jsx";
-import PrivateAdminRoute from "./components/PrivateAdminRoute.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' //--agregar rutas para enlaces
+import './index.css'
+import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateAdminRoute>
-              <Dashboard />
-            </PrivateAdminRoute>
-          }
-        />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 )
