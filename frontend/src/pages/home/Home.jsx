51b8fe0a10c2be12ae3b75b11dbecb3c2ac30e25
sara <sararuizn9@gmail.com>
2026-04-02 02:14:45 +0200

    proyecto arreglado

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,8 +2,15 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>este es el home.jsx</h1>
-    </div>
+    <>
+      <h3>'Tu videoclub favorito, modernizado para ti ;)'</h3>
+      <div className="description">
+        <p>Explora un catálogo digital modernizado y descubre una experiencia online creada para hacer más sencillo buscar, alquilar y disfrutar del mejor contenido.</p>
+        <img src="../../../public/imgs/fondo.avif" alt="Videoclub"></img>
+        <hr></hr>
+        <img src="../../../public/imgs/posters.avif" alt="Videoclub"></img>
+
+      </div>
+    </>
   );
 }
