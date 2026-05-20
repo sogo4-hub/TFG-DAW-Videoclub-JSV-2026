f9046b21c13398eac0b3bf4ab21a3d422e6b6f8c
sara <sararuizn9@gmail.com>
2026-05-20 20:13:11 +0200

    maldito form login actualizado

--- backend/src/main/java/es/daw/backend/repository/UsuarioRepository.java
+++ backend/src/main/java/es/daw/backend/repository/UsuarioRepository.java
@@ -6,4 +6,8 @@ import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
+
+    Optional<Usuario> findByNombre(String nombre);
+
+    Optional<Usuario> findByEmailOrNombre(String email, String nombre);
 }
